65c00d725a91feffb9d00dbc90aa2f7cf34335a6
Luca Narduzzi <luca.narduzzi@fmach.it>
2016-06-30 14:52:26 +0200

    new

--- CCC_method/R/ranking.2010.R
+++ CCC_method/R/ranking.2010.R
@@ -1,8 +1,9 @@
 #' @title Ranking function to get the best candidates from the metfRag analysis from .csv files
 #' @description The function reorders the metfrag candidates using the information given by the CCC_method. It uploads the .csv files from a directory, and performs reordering of the data inside each file. It returns a data.frame containing all the files together. NB: the .csv files must be from the web metfrag2010 and must be already converted in csv files (not xls). The name of the .csv file must correspond to the original raw number of the markers in the peakTable. 
 #' @param directory where the .csv files are stored. The file name must correspond to the feature number (rowname) in the original peaktable  
-#' @param tni is a list of grouped features given by the CCC.peakTable/CCC.xsaFA function 
-#' @usage ranking.2010(directory, tni)
+#' @param tni is a list of grouped features given by the CCC.peakTable/CCC.xsaFA function
+#' @param sep is the separator preferred in the kind of file (useful only for csv files). Default is the "comma". Any separator can be applied. 
+#' @usage ranking.2010(directory, tni, sep)
 #' @return a list of csv files reordered according to the CCC method parameters
 #' @export "ranking.2010"
 #' @author Luca Narduzzi "nardluca@gmail.com"
@@ -10,7 +11,7 @@
 #' data (peakTable)
 #' tni <- CCC.peakTable(peakTable, polarity = "negative")
 #' candidates <- ranking.2010(system.file("extdata", "2010", package = "CCC"), tni)
-ranking.2010 <- function(directory, tni) {
+ranking.2010 <- function(directory, tni, sep = ",") {
   setwd(directory)
   compi <- do.call(rbind, tni)
   row.names(compi) <- compi$rowname
@@ -22,7 +23,7 @@ ranking.2010 <- function(directory, tni) {
   candidate <- list()
   for (i in 1:length(fil)) {
     print(fil[i])
-    files <- read.csv(file[fi][i], stringsAsFactors=FALSE)
+    files <- read.csv(file[fi][i], sep = sep, stringsAsFactors=FALSE)
     name <- files[4,]
     files <- files[5:nrow(files),]
     names(files) <- name
